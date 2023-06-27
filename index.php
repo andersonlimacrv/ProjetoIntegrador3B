341f2d9d93b50f84af017a928b8ebd37515cf8db
Anderson_Carvalho <andersonlimacrv@gmail.com>
2023-06-27 18:27:36 -0300

    initial commit

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php
-date_default_timezone_set("America/Sao_Paulo");
-$dataAtual = date("Y-m-d");
-$horaAtual = date("H:i:s");
-echo "Data e hora atual: " . $dataAtual . ", " . $horaAtual;
+
+require __DIR__ . '/vendor/autoload.php';
