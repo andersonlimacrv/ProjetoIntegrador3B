7d8773ba422875f971cea24269215fe3bab0e003
Anderson_Carvalho <andersonlimacrv@gmail.com>
2023-07-04 22:30:57 -0300

    feat: adicionado rota de post das páginas.

--- routes/pages.php
+++ routes/pages.php
@@ -8,32 +8,57 @@ use \App\Controller\Pages\DonationListController;
 use \App\Controller\Pages\DonateController;
 
 
-
-
-
-//ROTA HOME
+//ROTA HOME GET
 $obRouter->get('/', [
     function () {
         return new Response(200, HomeController::getHome());
     }
 ]);
 
-// ROTA DE CADASTRO DE DOAÇÕES
+//ROTA HOME POST
+$obRouter->post('/', [
+    function () {
+        return new Response(200, HomeController::getHome());
+    }
+]);
+
+// ROTA DE CADASTRO DE DOAÇÕES GET
 $obRouter->get('/doar', [
     function () {
         return new Response(200, DonateController::getHome());
     }
 ]);
 
-// ROTA DE CADASTRO DE DOAÇÕES
+// ROTA DE CADASTRO DE DOAÇÕES POST 
+$obRouter->post('/doar', [
+    function () {
+        return new Response(200, DonateController::getHome());
+    }
+]);
+
+// ROTA DE VISUALIZAÇÃO DE DOAÇÕES GET
 $obRouter->get('/visualizar', [
     function () {
         return new Response(200, DonationListController::getHome());
     }
 ]);
 
-// ROTA DE CADASTRO DE DOAÇÕES
+// ROTA DE VISUALIZAÇÃO DE DOAÇÕES POST
+$obRouter->post('/visualizar', [
+    function () {
+        return new Response(200, DonationListController::getHome());
+    }
+]);
+
+// ROTA DE EDIÇÃO DE DOAÇÕES GET
 $obRouter->get('/editar', [
+    function () {
+        return new Response(200, DonationEditController::getHome());
+    }
+]);
+
+// ROTA DE EDIÇÃO DE DOAÇÕES POST
+$obRouter->post('/editar', [
     function () {
         return new Response(200, DonationEditController::getHome());
     }
