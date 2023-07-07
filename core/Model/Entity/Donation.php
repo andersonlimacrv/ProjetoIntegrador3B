94f031aa36a62e753b9fd6af5458585c5ece7cb0
Anderson_Carvalho <andersonlimacrv@gmail.com>
2023-07-07 19:41:09 -0300

    feat: Terminado post de formulário e colocado em todas as respectivas tabelas no banco.

--- core/Model/Entity/Donation.php
+++ core/Model/Entity/Donation.php
@@ -73,6 +73,7 @@ class Donation
         $cnpjName = $formData['cnpjName'];
         $donatedItemType = $formData['donatedItemType'];
         $description = $formData['description'];
+        $estimed_value = $formData['estimatedValue'];
         $currency = $formData['currency'];
         $amount = $formData['amount'];
         $brand = $formData['brand'];
@@ -80,12 +81,27 @@ class Donation
         $power = $formData['power'];
         $voltage = $formData['voltage'];
         $needsRepair = $formData['needsRepair'];
+        $type = $formData['type'];
+        $foodTaste = $formData['taste'];
+        $ingredients = $formData['ingredients'];
+        $expiryDate = $formData['expiryDate'];
+        $unit = $formData['unit'];
+        $quantity = $formData['quantity'];
+        $foodIsPerishable = $formData['isPerishable'];
+        $isAlcoholic = $formData['isAlcoholic'];
+        $isCarbonated = $formData['isCarbonated'];
+        $clothingColor = $formData['color'];
+        $condition = $formData['condition'];
+        $clothingSize = $formData['size'];
+        $clothingGender = $formData['gender'];
+        $clothingAgeRange = $formData['ageRange'];
+        $weight = $formData['weight'];
+        $usage = $formData['usage'];
+        $dimensions = $formData['dimensions'];
+        $material = $formData['material'];
+        $allergens = $formData['allergens'];
 
 
-        echo "<pre>";
-        print_r($formData);
-        echo "</pre>";
-        exit;
 
         // Inserir dados no banco de dados
         $db->beginTransaction();
@@ -124,29 +140,78 @@ class Donation
                 $stmt->execute([$currency, $amount]);
                 $moneyId = $db->lastInsertId();
                 $donatedItemReferenceId = '';
+            } else if ($donatedItemType === 'ApplianceItem') {
+                // Inserir na tabela items
+                $stmt = $db->prepare("INSERT INTO items (item_type, description, estimed_value) VALUES (?, ?, ?)");
+                $stmt->execute([$donatedItemType, $description, $estimed_value]);
+                $item_id = $db->lastInsertId();
+
+                // Inserir appliance_items - ELETRODOMÉSTICO:
+                $stmt = $db->prepare("INSERT INTO appliance_items (item_id, brand, model, power, voltage, needsRepair) VALUES (?, ?, ?, ?, ?, ?)");
+                $stmt->execute([$item_id, $brand, $model, $power, $voltage, $needsRepair]);
+                $donatedItemReferenceId = $db->lastInsertId();
+            } else if ($donatedItemType === 'ClothingItem') {
+
+                // Inserir na tabela items
+                $stmt = $db->prepare("INSERT INTO items (item_type, description, estimed_value) VALUES (?, ?, ?)");
+                $stmt->execute([$donatedItemType, $description, $estimed_value]);
+                $item_id = $db->lastInsertId();
+                // Inserir na tabela clothing_items
+                $stmt = $db->prepare("INSERT INTO clothing_items (item_id, type, color, condition, size, gender, age_range) VALUES (?, ?, ?, ?, ?, ?, ?)");
+                $stmt->execute([$item_id, $type, $clothingColor, $condition, $clothingSize, $clothingGender, $clothingAgeRange]);
+                $donatedItemReferenceId = $db->lastInsertId();
+            } else if ($donatedItemType === 'GenericItem') {
+
+                // Inserir na tabela items
+                $stmt = $db->prepare("INSERT INTO items (item_type, description, estimed_value) VALUES (?, ?, ?)");
+                $stmt->execute([$donatedItemType, $description, $estimed_value]);
+                $item_id = $db->lastInsertId();
+
+                //Inserir generic_items - VEICULO:
+                $stmt = $db->prepare("INSERT INTO generic_items (item_id, description) VALUES (?, ?)");
+                $stmt->execute([$item_id, $description]);
+                $donatedItemReferenceId = $db->lastInsertId();
+            } else if ($donatedItemType === 'FoodItem') {
+
+                // Inserrir na tabela items
+                $stmt = $db->prepare("INSERT INTO items (item_type, description, estimed_value) VALUES (?, ?, ?)");
+                $stmt->execute([$donatedItemType, $description, $estimed_value]);
+                $item_id = $db->lastInsertId();
+
+                // Inserir na tabela food_items
+                $stmt = $db->prepare("INSERT INTO food_items (item_id, type, quantity, unit, expiry_date, ingredients, allergens, taste, is_perishable) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                $stmt->execute([$item_id, $type, $quantity, $unit, $expiryDate, $ingredients, $allergens, $foodTaste, $foodIsPerishable]);
+                $donatedItemReferenceId = $db->lastInsertId();
+            } else if ($donatedItemType === 'DrinkItem') {
+
+                // Inserir na tabela items
+                $stmt = $db->prepare("INSERT INTO items (item_type, description, estimed_value) VALUES (?, ?, ?)");
+                $stmt->execute([$donatedItemType, $description, $estimed_value]);
+                $item_id = $db->lastInsertId();
+
+                // Inserir na tabela drink_items
+                $stmt = $db->prepare("INSERT INTO drink_items (item_id, type, quantity, unit, expiry_date, ingredients, is_carbonated, is_alcoholic) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+                $stmt->execute([$item_id, $type, $quantity, $unit, $expiryDate, $ingredients, $isCarbonated, $isAlcoholic]);
+                $donatedItemReferenceId = $db->lastInsertId();
+            } else if ($donatedItemType === 'FurnitureItem') {
+
+                // Inserir na tabela items
+                $stmt = $db->prepare("INSERT INTO items (item_type, description, estimed_value) VALUES (?, ?, ?)");
+                $stmt->execute([$donatedItemType, $description, $estimed_value]);
+                $item_id = $db->lastInsertId();
+
+                // Inserir na tabela furniture_items
+                $stmt = $db->prepare("INSERT INTO furniture_items (item_id, brand, material, condition, dimensions, weight, usage) VALUES (?, ?, ?, ?, ?, ?, ?)");
+                $stmt->execute([$item_id, $brand, $material, $condition, $dimensions, $weight, $usage]);
+                $donatedItemReferenceId = $db->lastInsertId();
             } else {
-                $moneyId = '';
             }
 
             // Inserir doação na tabela donations
             $donationDate = date('Y-m-d');
             $stmt = $db->prepare("INSERT INTO donations (donorId, donatedItemType, donatedItemReferenceId, donationDate, moneyId) VALUES (?, ?, ?, ?, ?)");
             $stmt->execute([$donorId, $donatedItemType, $donatedItemReferenceId, $donationDate, $moneyId]);
-
-            //Inserir Item - ELETRODOMÉSTICO:
-
-            if ($donorType === 'ApplianceItem') {
-                $stmt = $db->prepare("INSERT INTO appliance_items (donorType) VALUES ('anonymous')");
-                $stmt->execute();
-                $donorId = $db->lastInsertId();
-
-                // Inserir na tabela anonymous_donors
-                $stmt = $db->prepare("INSERT INTO anonymous_donors (id) VALUES (?)");
-                $stmt->execute();
-            } else if ($donorType === '') {
-            }
-
-
+            $item_id = $db->lastInsertId();
 
 
             $db->commit();
